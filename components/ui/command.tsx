1af02d801e6cf4b2ee1d8e105ee0d1ae05dccf19
suptechie <suptechie@outlook.com>
2024-10-29 20:43:23 +0530

    Unused var issues solved

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -23,7 +23,7 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-//eslint-disable-next-line react/display-name
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface CommandDialogProps extends DialogProps {}
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
