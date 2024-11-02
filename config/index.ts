a4d3a7ec2fd99a4a6de038947774418ea0509595
suptechie <suptechie@outlook.com>
2024-11-02 13:37:30 +0530

    dummy url added to config

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 export const MONGO_URI = process.env.MONGO_URI as string;
 export const NODE_ENV = process.env.NODE_ENV as string;
 export const TOKEN_SECRET = process.env.TOKEN_SECRET as string;
-export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL as string
+export const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL ?? "https://readify-gamma.vercel.app";
 export const CLOUDINARY_URL= process.env.CLOUDINARY_URL as string
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET as string
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY as string
